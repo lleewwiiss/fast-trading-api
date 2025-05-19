481ad12b7c7eb25eee87e7d8c9374dfaf03f56ad
iam4x <iam4x@me.com>
2025-05-19 22:20:36 +0400

    refactor(bybit.api): use request util

--- src/exchanges/bybit/bybit.api.ts
+++ src/exchanges/bybit/bybit.api.ts
@@ -2,21 +2,8 @@ import { createHMAC, createSHA256 } from "hash-wasm";
 
 import { BROKER_ID, RECV_WINDOW } from "./bybit.config";
 
-import { retry } from "~/utils/retry.utils";
 import { stringify } from "~/utils/query-string.utils";
-
-type Request = {
-  key: string;
-  secret: string;
-  url: string;
-  method?: "GET" | "POST";
-  params?: Record<string, string | number | string[] | number[]>;
-  body?: Record<
-    string,
-    string | number | string[] | number[] | Array<Record<string, any>>
-  >;
-  retries?: number;
-};
+import { type Request, request } from "~/utils/request.utils";
 
 export const bybitWebsocketAuth = async ({
   key,
@@ -31,42 +18,32 @@ export const bybitWebsocketAuth = async ({
   return [key, expires.toFixed(0), signature];
 };
 
-export const bybit = async <T>(request: Request) => {
-  return retry(async () => {
-    const timestamp = new Date().getTime();
+export const bybit = async <T>(
+  req: Request & { key: string; secret: string },
+) => {
+  const hmac = await createHMAC(createSHA256(), req.secret);
 
-    const url = request.params
-      ? `${request.url}?${stringify(request.params)}`
-      : request.url;
+  const timestamp = new Date().getTime();
+  const signature = hmac
+    .init()
+    .update(
+      [
+        timestamp,
+        req.key,
+        RECV_WINDOW,
+        req.params ? stringify(req.params) : "",
+        req.body ? JSON.stringify(req.body) : "",
+      ].join(""),
+    )
+    .digest("hex");
 
-    const hmac = await createHMAC(createSHA256(), request.secret);
-    const signature = hmac
-      .init()
-      .update(
-        [
-          timestamp,
-          request.key,
-          RECV_WINDOW,
-          request.params ? stringify(request.params) : "",
-          request.body ? JSON.stringify(request.body) : "",
-        ].join(""),
-      )
-      .digest("hex");
+  const headers = {
+    "X-BAPI-SIGN": signature,
+    "X-BAPI-API-KEY": req.key,
+    "X-BAPI-TIMESTAMP": `${timestamp}`,
+    "X-BAPI-RECV-WINDOW": `${RECV_WINDOW}`,
+    "X-Referer": BROKER_ID,
+  };
 
-    const response = await fetch(url, {
-      method: request.method || "GET",
-      body: request.body ? JSON.stringify(request.body) : undefined,
-      headers: {
-        "X-BAPI-SIGN": signature,
-        "X-BAPI-API-KEY": request.key,
-        "X-BAPI-TIMESTAMP": `${timestamp}`,
-        "X-BAPI-RECV-WINDOW": `${RECV_WINDOW}`,
-        "X-Referer": BROKER_ID,
-      },
-    });
-
-    const json: T = await response.json();
-
-    return json;
-  }, request.retries ?? 0);
+  return request<T>({ ...req, headers });
 };
